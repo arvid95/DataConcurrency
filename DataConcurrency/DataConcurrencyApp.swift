abe46cf0c88139aeb20befc457b9e591a3ebac22
Arvid <axar1802@student.ju.se>
2022-11-13 16:44:05 +0100

    Initial Commit

---
+++ DataConcurrency/DataConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataConcurrencyApp.swift
+//  DataConcurrency
+//
+//  Created by Arvid Axelsson on 2022-11-13.
+//
+
+import SwiftUI
+
+@main
+struct DataConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
